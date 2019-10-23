be43970a8d8a45de98d9cbe6b7b4c4e1c76a7759
Pablo Espinosa <espipj@gmail.com>
2019-10-23 02:01:23 +0100

    Updating deployment script #14
    Added username in the gh-pages script and build web step

--- src/deploy-gh-pages.js
+++ src/deploy-gh-pages.js
@@ -3,6 +3,10 @@ var ghpages = require('gh-pages');
 ghpages.publish(
   'dist-web',
   {
+    user: {
+      name: 'Pablo Espinosa',
+      email: 'espipj@gmail.com',
+    },
     repo:
       'https://' +
       process.argv.slice(2)[0] +
