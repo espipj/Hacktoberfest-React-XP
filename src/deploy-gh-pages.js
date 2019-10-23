ecaf1744fe4f3f9505758f32bf03e9f367f05f7a
Pablo Espinosa <espipj@gmail.com>
2019-10-23 01:54:56 +0100

    Added user

--- src/deploy-gh-pages.js
+++ src/deploy-gh-pages.js
@@ -3,6 +3,10 @@ console.log(process.argv.slice(2)[0].substring(1, 4));
 ghpages.publish(
   'dist-web',
   {
+    user: {
+      name: 'Pablo Espinosa',
+      email: 'espipj@gmail.com',
+    },
     repo:
       'https://' +
       process.argv.slice(2)[0] +
