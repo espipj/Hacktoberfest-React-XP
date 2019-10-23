da612cd6266299db14d4b391b0692aba6eecd690
Pablo Espinosa <espipj@gmail.com>
2019-10-23 01:59:29 +0100

    Added user to fix deployment

--- src/deploy-gh-pages.js
+++ src/deploy-gh-pages.js
@@ -1,5 +1,5 @@
 var ghpages = require('gh-pages');
-console.log(process.argv.slice(2)[0].substring(1, 4));
+
 ghpages.publish(
   'dist-web',
   {
@@ -11,6 +11,7 @@ ghpages.publish(
       'https://' +
       process.argv.slice(2)[0] +
       '@github.com/espipj/Hacktoberfest.git',
+    silent: true,
   },
   () => console.log('Successfully published!')
 );
